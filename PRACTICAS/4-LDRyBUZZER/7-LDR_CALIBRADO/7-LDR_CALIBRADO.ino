2145f6f61dcc9a990fb8bfb1eb3887dde12f6283
enriquebrito <58968001+enriquebrito@users.noreply.github.com>
2020-06-01 17:23:41 +0200

    Add files via upload

--- PRACTICAS/4-LDRyBUZZER/7-LDR_CALIBRADO/7-LDR_CALIBRADO.ino
+++ PRACTICAS/4-LDRyBUZZER/7-LDR_CALIBRADO/7-LDR_CALIBRADO.ino
@@ -1,14 +1,12 @@
 void setup() {
-// initialize serial communication
-at 9600 bits per second:
-Serial.begin(9600);
+	// initialize serial communication at 9600 bits per second:
+	Serial.begin(9600);
 }
-// the loop routine runs over and
-over again forever:
+// the loop routine runs over and over again forever:
 void loop() {
-// read the input on analog pin 0:
-int sensorValue = analogRead(A0);
-// print out the value you read:
-Serial.println(sensorValue);
-delay(1); // delay in between reads for stability
+	// read the input on analog pin 0:
+	int sensorValue = analogRead(A0);
+	// print out the value you read:
+	Serial.println(sensorValue);
+	delay(1); // delay in between reads for stability
 }
